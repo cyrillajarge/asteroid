cab253ec9dff94e222a9c4360e1d9cef91381f20
Paul <rahveiz@gmail.com>
2020-01-27 10:33:25 +0100

    window class

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 
 #include "SDL2/SDL.h"
+#include "GameWindow.hpp"
 #include "Spaceship.hpp"
 #include "Rocket.hpp"
 
@@ -22,64 +23,18 @@ int main(int argc, char* argv[]) {
       return EXIT_FAILURE;
   }
 
-  SDL_Window *window = SDL_CreateWindow("Asteroid", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
-
-  if( window == NULL){
-    cout << "Could not create window: " << SDL_GetError() << endl;
-    return EXIT_FAILURE;
-  }
-
-  SDL_Renderer* renderer = NULL;
-  renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+  GameWindow *window = new GameWindow("Hemoroids", WIDTH, HEIGHT);
 
   vec2 position = vec2(WIDTH / 2.0f, HEIGHT / 2.0f);
-  Spaceship* uss_enterprise = new Spaceship(position,renderer);
+  window->InitShip(position);
 
-  vec2 rocket_pos = vec2(40.0f,40.0f);
-  Rocket* rocket = new Rocket(rocket_pos);
+  // vec2 rocket_pos = vec2(40.0f,40.0f);
+  // Rocket* rocket = new Rocket(rocket_pos);
   // rocket->draw(renderer);
+  window->MainLoop();
   
-  int lastTime = 0, currentTime = 0;
-
-  double deltaTime = 0.0f;
-  double deltaRotation = 0.0f;
-
-  SDL_Event windowEvent;
-  while(true){
-    if(SDL_PollEvent( &windowEvent)){
-      if(SDL_QUIT == windowEvent.type){
-        break;
-      }
-    }
-    currentTime = SDL_GetTicks();
-    deltaTime = currentTime - lastTime;
-    if (deltaTime > 30) /* Si 30 ms se sont écoulées */
-    {
-        const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-        if (keystates[SDL_SCANCODE_LEFT]) {
-          deltaRotation = -DELTA_ANGLE;
-        }
-        if (keystates[SDL_SCANCODE_RIGHT]) {
-          deltaRotation = DELTA_ANGLE;
-        }
-        if(keystates[SDL_SCANCODE_UP]) {
-          uss_enterprise->activateBoost();
-        }
-        if(keystates[SDL_SCANCODE_SPACE]){
-          uss_enterprise->fireRocket();
-        }
-        uss_enterprise->update(deltaRotation, WIDTH, HEIGHT);
-        uss_enterprise->draw();
-        
-        deltaRotation = 0.0f;
-        uss_enterprise->deactivateBoost();
-        lastTime = currentTime;
-    }
-  }
-
-  SDL_DestroyWindow(window);
+  delete window;
   SDL_Quit();
-  delete uss_enterprise;
   return EXIT_SUCCESS;
 }
 
