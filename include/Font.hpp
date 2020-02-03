76981a4210da7c13d1077219809d1fc4111bafa2
LAJARGE CYRIL <cyril.lajarge@etu.unistra.fr>
2020-02-03 14:59:17 +0100

    basic files for font

---
+++ include/Font.hpp
@@ -0,0 +1,15 @@
+#ifndef FONT_H
+#define FONT_H
+
+#include "Hershey.hpp"
+
+class Font{
+  public:
+
+  public:
+    Font();
+    ~Font();
+    void drawLetter(char letter);
+};
+
+#endif
---
+++ src/Font.cpp
@@ -0,0 +1,13 @@
+#include "Font.hpp"
+
+Font::Font(){
+
+}
+
+Font::~Font(){
+
+}
+
+void Font::drawLetter(char letter){
+
+}
