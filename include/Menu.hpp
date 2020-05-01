16a65ce24f9b35f40d8182c2d25cebf6714447fb
Rahveiz <rahveiz@gmail.com>
2020-05-01 14:19:49 +0200

    add some comments

--- include/Menu.hpp
+++ include/Menu.hpp
@@ -24,10 +24,45 @@ public:
 public:
   Menu(Font *font);
   ~Menu();
+  /**
+   * @brief Adds a new Button to the menu
+   * 
+   * @param tag 
+   * @param label 
+   * @param position 
+   */
   void addButton(std::string tag, std::string label, glm::vec2 position);
+  /**
+   * @brief Adds a new Checkbox to the menu
+   * 
+   * @param tag 
+   * @param label 
+   * @param position 
+   */
   void addCheckbox(std::string tag, std::string label, glm::vec2 position);
+  /**
+   * @brief Adds a new PlainText component to the menu
+   * 
+   * @param tag 
+   * @param label 
+   * @param position 
+   */
   void addPlainText(std::string tag, std::string label, glm::vec2 position);
+
+  /**
+   * @brief Adds a new TextInput to the menu
+   * 
+   * @param tag 
+   * @param label 
+   * @param position 
+   */
   void addTextInput(std::string tag, std::string label, glm::vec2 position);
+
+  /**
+   * @brief Draws the menu
+   * 
+   * @param renderer 
+   */
   void draw(SDL_Renderer *renderer);
 };
 
